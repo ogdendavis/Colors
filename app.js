69dcfa513e3762691de0126f2bcfbe525fd2c85e
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-01-12 17:07:39 -0500

    Add basic working input fields

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
 const app = document.querySelector('.app');
 app.innerHTML = 'Hello, world!';
+
+const input = document.createElement('form');
+app.appendChild(input);
+
+const red = document.createElement('input');
+input.appendChild(red);
+
+const green = document.createElement('input');
+input.appendChild(green);
+
+const blue = document.createElement('input');
+input.appendChild(blue);
+
+
+function updateColors() {
+  values = {
+    red: red.value,
+    green: green.value,
+    blue: blue.value,
+  }
+  redy = red.value > 255 ? red.value % 255 : red.value;
+  greeny = green.value > 255 ? green.value % 255 : green.value;
+  bluey = blue.value > 255 ? blue.value % 255 : blue.value;
+
+  app.style.background = `rgb(${redy || 0},${greeny || 0},${bluey || 0})`;
+}
+
+red.addEventListener('change', updateColors);
+green.addEventListener('change', updateColors);
+blue.addEventListener('change', updateColors);
