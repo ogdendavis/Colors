b108ad08f7dbc4800603f4b95ac21cc9be22f088
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-01-12 17:19:34 -0500

    Add slider labels

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
 const app = document.querySelector('.app');
-app.innerHTML = 'Hello, world!';
 
 const input = document.createElement('form');
+input.id = 'rgb';
 app.appendChild(input);
 
 const red = document.createElement('input');
+red.setAttribute('name', 'red');
 red.setAttribute('type', 'range');
 red.setAttribute('min', '0');
 red.setAttribute('max', '255');
 red.setAttribute('value', '0');
+
+const redLabel = document.createElement('label');
+redLabel.setAttribute('for', 'red');
+redLabel.innerText = 'Red';
+
+input.appendChild(redLabel);
 input.appendChild(red);
 
 const green = document.createElement('input');
+green.setAttribute('name', 'green');
 green.setAttribute('type', 'range');
 green.setAttribute('min', '0');
 green.setAttribute('max', '255');
 green.setAttribute('value', '0');
+
+const greenLabel = document.createElement('label');
+greenLabel.setAttribute('for', 'green');
+greenLabel.innerText = 'Green';
+
+input.appendChild(greenLabel);
 input.appendChild(green);
 
 const blue = document.createElement('input');
+blue.setAttribute('name', 'blue');
 blue.setAttribute('type', 'range');
 blue.setAttribute('min', '0');
 blue.setAttribute('max', '255');
 blue.setAttribute('value', '0');
+
+const blueLabel = document.createElement('label');
+blueLabel.setAttribute('for', 'blue');
+blueLabel.innerText = 'Blue';
+
+input.appendChild(blueLabel);
 input.appendChild(blue);
 
 
