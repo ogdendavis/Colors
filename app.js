a5aeea89c2e8390b08c62f1aebeec50d630efc85
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-01-14 08:56:58 -0500

    Add HSL values

--- app.js
+++ app.js
@@ -52,6 +52,8 @@ app.appendChild(rgbValues);
 const hexValues = document.createElement('div');
 app.appendChild(hexValues);
 
+const hslValues = document.createElement('div');
+app.appendChild(hslValues);
 
 function updateColors() {
   values = {
@@ -70,15 +72,38 @@ function updateColors() {
   const rgb = `rgb(${values.rgb.red || 0}, ${values.rgb.green || 0}, ${values.rgb.blue || 0})`
   const hex = `#${values.hex.red.length > 1 ? values.hex.red : '0' + values.hex.red}${values.hex.green.length > 1 ? values.hex.green : '0' + values.hex.green}${values.hex.blue.length > 1 ? values.hex.blue : '0' + values.hex.blue}`
 
-
-
+  const hslRaw = hslCalc(values);
+  values.hsl = hslRaw;
+  const hsl = `hsl(${hslRaw.h}, ${hslRaw.s}%, ${hslRaw.l}%)`;
 
 
   app.style.background = rgb;
   rgbValues.innerText = rgb;
   hexValues.innerText = hex;
+  hslValues.innerText = hsl;
 
   console.log(values);
+}
+
+function hslCalc(values) {
+  // Figuring out HSL. Based on http://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/
+  const redRatio = Number(values.rgb.red) / 255;
+  const greenRatio = Number(values.rgb.green) / 255;
+  const blueRatio = Number(values.rgb.blue) / 255;
+  const ratioMax = Math.max(redRatio, greenRatio, blueRatio);
+  const ratioMin = Math.min(redRatio, greenRatio, blueRatio);
+  const lRatio = (ratioMax + ratioMin) / 2;
+  const sRatio = ratioMax === ratioMin ? 0 :
+                    lRatio > 0.5 ? (ratioMax - ratioMin)/(2 - ratioMax - ratioMin) :
+                                   (ratioMax - ratioMin)/(ratioMax + ratioMin);
+  const hRatio = redRatio === ratioMax ? (greenRatio - blueRatio) / (ratioMax - ratioMin) :
+           greenRatio === ratioMax ? 2 + (blueRatio - redRatio) / (ratioMax - ratioMin) :
+             blueRatio === ratioMax ? 4 + (redRatio - greenRatio) / (ratioMax - ratioMin) : 0;
+  return {
+    h: Math.round(hRatio * 60),
+    s: Math.round(sRatio * 100),
+    l: Math.round(lRatio * 100),
+  }
 }
 
 red.addEventListener('change', updateColors);
